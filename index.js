d908aa33108797e99001f5ba7fe76c04b6c1b39f
Noah Cote <noahcote10@gmail.com>
2022-11-29 18:38:31 -0500

    fixed inquirer promts. added license info object. added functions to render license badge, info, etc. testing app gets through questions, but does not render readme.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = inquirer
-    .prompt([
+const questions =[
         {
             type: 'input',
             name: 'title',
@@ -15,51 +15,55 @@ const questions = inquirer
             message: 'Enter the description of your repository.'
         },
         {
-            type: 'confirm',
-            name: 'confirm-toc',
-            message: 'Do you need a Table of Contents? (Y/N)'
-        },
-        // type: 'checkbox',
-        // name: 'toc',
-        // message: 'Select your Table of Contents.'
-        // choices: ['Installation','Usage', 'License', 'Contributing', 'Tests', 'Questions']
-        {
             type: 'input',
             name: 'installation',
-            message: 'Enter installation instructions here.'
+            message: 'Enter installation instructions.'
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'Enter usage information here.'
+            message: 'Enter usage information.'
         },
         {
             type: 'input',
             name: 'contributing',
-            message: 'Enter contributing instructions here.'
+            message: 'Enter contributing instructions.'
         },
         {
             type: 'input',
             name: 'tests',
-            message: 'Enter testing instructions here.'
+            message: 'Enter testing instructions.'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license.',
+            choices: ['MIT','Mozilla Public License 2.0','Apache License 2.0','None']
         },
         {
             type: 'input',
             name: 'questions-github',
-            message: 'Enter your GitHub username here.'
-            // use github API to pull user link
+            message: 'Enter your GitHub username.'
         },
         {
             type: 'input',
             name: 'questions-email',
-            message: 'Enter email and contact instructions here.'
+            message: 'Enter email and contact instructions.'
         },
-    ])
+    ]
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function createReadMe(name, input) {
+    fs.writeFile(name, input, (err) => 
+    err ? console.error(err) : console.log ("ReadMe successfully created"))
+ }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() { 
+    inquirer.prompt(questions).then((data) => {
+        const content = generateMarkdown(data);
+        createReadMe("README.md", content)
+    })
+}
 
 // Function call to initialize app
 init();
